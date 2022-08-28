5d8dab436f9fac5517fc32de0b1119e358a89294
Simon Schneegans <code@simonschneegans.de>
2022-08-28 20:51:35 +0200

    :wrench: Increase test timeouts

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -141,11 +141,12 @@ test_effect() {
   set_setting "open-preview-effect" "${1}"
   set_setting "close-preview-effect" "${1}"
 
+  sleep 1
   do_in_pod gnome-extensions prefs "${EXTENSION}"
-  sleep 3
+  sleep 4
   compare_with_target "tests/references/${1}-open-${SESSION}-${FEDORA_VERSION}.png" "Failed to test ${1} window open effect!"
   send_keystroke "Alt+F4"
-  sleep 3
+  sleep 4
   compare_with_target "tests/references/${1}-close-${SESSION}-${FEDORA_VERSION}.png" "Failed to test ${1} window close effect!"
 }
 
