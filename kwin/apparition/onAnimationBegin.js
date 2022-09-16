90c9258d75605ac836c78ed842376e386ab0fc93
Simon Schneegans <code@simonschneegans.de>
2022-09-16 17:54:21 +0200

    :wrench: Test whether the new test can fail

--- kwin/apparition/onAnimationBegin.js
+++ kwin/apparition/onAnimationBegin.js
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-late
 
 // This part is automatically included in the effect's source during the build process.
 // The code below is called whenever a window is closed or opened.
