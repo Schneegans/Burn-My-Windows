dc9ba82a3da07bd064fad09cd89ceb5e4e88f886
Simon Schneegans <code@simonschneegans.de>
2023-07-04 21:55:42 +0200

    :green_heart: Also set GSK_RENDERER when generating references

--- tests/generate-references.sh
+++ tests/generate-references.sh
@@ -124,6 +124,13 @@ capture_effect() {
   capture "tests/references/${1}-close-${SESSION}-${FEDORA_VERSION}.png" "${EFFECT_CROP}"
 }
 
+
+# -------------------------------------------------------------- set GSK_RENDERER to cairo
+
+echo "Make sure to use Cairo GTK rendering backend."
+do_in_pod 'echo "export GSK_RENDERER=cairo" >> .bash_profile'
+
+
 # ----------------------------------------------------- wait for the container to start up
 
 echo "Waiting for D-Bus."
