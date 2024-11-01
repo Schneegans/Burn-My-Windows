89c8b760cda6f45f883dc3be1bd75e0e1ef4a951
Justin Garza <jgarza9788@gmail.com>
2024-10-31 21:48:04 -0400

    Update prefs.js

--- prefs.js
+++ prefs.js
@@ -180,27 +180,26 @@ export default class BurnMyWindowsPreferences extends ExtensionPreferences {
 
     // search for effect feature
     this._searchEntry = this._builder.get_object('search_entry');
-    this._searchEntry.connect(
-        'search-changed', () => {
-          const query = this._searchEntry.get_text().toLowerCase();
+    this._searchEntry.connect('search-changed', () => {
+      const query = this._searchEntry.get_text().toLowerCase();
 
-          this._effectRows.forEach(er => {
-            if (query === '') {
-              er.show();  // Show all effects if query is empty
-            } else {
-              // Show or hide each effect based on query match
-              const showEffect = er.name.toLowerCase().includes(query);
-              showEffect ? er.show() : er.hide();
+      this._effectRows.forEach(er => {
+        if (query === '') {
+          er.show();  // Show all effects if query is empty
+        } else {
+          // Show or hide each effect based on query match
+          const showEffect = er.name.toLowerCase().includes(query);
+          showEffect ? er.show() : er.hide();
 
-              // TODO
-              // maybe add a fuzzy search later
-              /*
-              const showEffect = utils.fuzzyMatch(er.name.toLowerCase(),
-              query.toLowerCase()); showEffect ? er.show() : er.hide();
-              */
-            }
-          });
-        });
+          // TODO
+          // maybe add a fuzzy search later
+          /*
+          const showEffect = utils.fuzzyMatch(er.name.toLowerCase(),
+          query.toLowerCase()); showEffect ? er.show() : er.hide();
+          */
+        }
+      });
+    });
 
     // Then add a preferences group for the effect expander rows.
     const group = this._builder.get_object('effects-group');
