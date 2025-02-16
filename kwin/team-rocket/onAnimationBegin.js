b4418a71c6e0a887c477fb1ade1755bccd1959a6
Simon Schneegans <code@simonschneegans.de>
2025-02-16 07:03:13 +0100

    :sparkles: Apply clang-format

--- kwin/team-rocket/onAnimationBegin.js
+++ kwin/team-rocket/onAnimationBegin.js
@@ -4,5 +4,5 @@
 // This part is automatically included in the effect's source during the build process.
 // The code below is called whenever a window is closed or opened.
 
-let seed     = [Math.random(), Math.random()];
+let seed = [Math.random(), Math.random()];
 effect.setUniform(this.shader, 'uSeed', seed);
