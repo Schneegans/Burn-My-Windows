c8cdbacf520856ffc2ed1af3cd37bf2556b6f204
Yassin Omer <yassinomer.work@gmail.com>
2025-10-03 17:18:03 +0600

    Update onSettingsChanged.js

--- kwin/fire/onSettingsChanged.js
+++ kwin/fire/onSettingsChanged.js
@@ -5,17 +5,15 @@
 // The code below is called whenever the user changes something in the configuration of
 // the effect.
 
+// clang-format off
 effect.setUniform(this.shader, 'uDuration', this.duration * 0.001);
-effect.setUniform(this.shader, 'uScale',
-                  effect.readConfig('Scale', 1.0));
-effect.setUniform(this.shader, 'uMovementSpeed',
-                  effect.readConfig('MovementSpeed', 1.0));
-effect.setUniform(this.shader, 'u3DNoise',
-                  effect.readConfig('3DNoise', true) ? 1.0 : 0.0);
-effect.setUniform(this.shader, 'uRandomColor',
-                  effect.readConfig('RandomColor', true) ? 1.0 : 0.0);
+effect.setUniform(this.shader, 'uScale', effect.readConfig('Scale', 1.0));
+effect.setUniform(this.shader, 'uMovementSpeed', effect.readConfig('MovementSpeed', 1.0));
+effect.setUniform(this.shader, 'u3DNoise',effect.readConfig('3DNoise', true) ? 1.0 : 0.0);
+effect.setUniform(this.shader, 'uRandomColor', effect.readConfig('RandomColor', true) ? 1.0 : 0.0);
 effect.setUniform(this.shader, 'uGradient1', this.readRGBAConfig('Gradient1'));
 effect.setUniform(this.shader, 'uGradient2', this.readRGBAConfig('Gradient2'));
 effect.setUniform(this.shader, 'uGradient3', this.readRGBAConfig('Gradient3'));
 effect.setUniform(this.shader, 'uGradient4', this.readRGBAConfig('Gradient4'));
 effect.setUniform(this.shader, 'uGradient5', this.readRGBAConfig('Gradient5'));
+// clang-format on
